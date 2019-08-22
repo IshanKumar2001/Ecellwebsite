66fb65103212a9640505ea1e89f1d2eef3301e5b
Anmol Srivastava <anmol150srivastava@gmail.com>
2019-08-22 15:03:22 +0530

    Init

---
+++ include/php/menubar.php
@@ -0,0 +1,152 @@
+<header id="header" class="page-section full-header">
+	<div id="header-wrap">
+		<div class="container clearfix">
+			<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+			<div id="logo">
+				<a href="index.php" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo.png" alt="Canvas Logo"></a>
+				<a href="index.php" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo%402x.png" alt="Canvas Logo"></a>
+			</div>
+			<nav id="primary-menu" class="dark">
+				<ul>
+					<li class="current">
+						<a href="index.php">
+							<div>Home</div>
+						</a>
+						<ul>
+							<li>
+								<a href="#section-abou">
+									<div>About us</div>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<div>Vision</div>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<div>Team</div>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<div>Partners</div>
+								</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">
+							<div>Initiatives</div>
+						</a>
+						<ul>
+							<li>
+								<a href="Start-ups.html">
+									<div>Start-ups</div>
+								</a>
+							<li>
+								<a href="Portfolios.html">
+									<div>Portfolios</div>
+								</a>
+							</li>
+							<li>
+								<a href="Start-up Service Program.html">
+									<div>Start-up Service Program</div>
+								</a>
+							</li>
+							<li>
+								<a href="Campus Ambasssdors.html">
+									<div>Campus Ambasssdors</div>
+								</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="index.html">
+							<div>Events</div>
+						</a>
+						<ul>
+							<li>
+								<a href="Events Calendar.html">
+									<div>Events Calendar</div>
+								</a>
+							</li>
+							<li>
+								<a href="E-Conclave.html">
+									<div>E-Conclave</div>
+								</a>
+							</li>
+							<li>
+								<a href="Industrial Visits.html">
+									<div>Industrial Visits</div>
+								</a>
+							</li>
+							<li>
+								<a href="Minor Events.html">
+									<div>Minor Events</div>
+								</a>
+							</li>
+							<li>
+								<a href="Internfair.html">
+									<div>Internfair</div>
+								</a>
+							</li>
+							<li>
+								<a href="Innovriti.html">
+									<div>Innovriti</div>
+								</a>
+							</li>
+							<li>
+								<a href="Past Speakers.html">
+									<div>Past Speakers</div>
+								</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="Blogs.html">
+							<div>Blogs</div>
+						</a>
+					</li>
+					<li>
+						<a href="Gallery.html">
+							<div>Gallery</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div>Get Involved</div>
+						</a>
+						<ul>
+							<li>
+								<a href="As MUJ Student.html">
+									<div>As MUJ Student</div>
+								</a>
+							</li>
+							<li>
+								<a href="As non-MUJ Students.html">
+									<div>As non-MUJ Students</div>
+								</a>
+							</li>
+							<li>
+								<a href="Alumni.html">
+									<div>Alumni</div>
+								</a>
+							</li>
+							<li>
+								<a href="Corporate.html">
+									<div>Corporate</div>
+								</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="Contact Us.html">
+							<div>Contact Us</div>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</header>
